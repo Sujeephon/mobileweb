5742365c33e2f1a1227b7e0190a09b7356611968
sujeephon <sujeephon.p@kkumail.com>
2026-02-03 19:04:45 +0700

    Add Lab 5 correctly (remove nested git)

---
+++ apps/lab05-photo-react/src/pages/Tab2.tsx
@@ -0,0 +1,52 @@
+// CHANGE: Add the following import
+import { camera } from 'ionicons/icons';
+// CHANGE: Update the following import
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/react';
+// CHANGE: Remove or comment out `ExploreContainer`
+// import ExploreContainer from '../components/ExploreContainer';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+  const { photos, addNewToGallery } = usePhotoGallery();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle size="small">Lab 05 - โดย ศุจีภรณ์ รหัส 663380613-0</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+            <IonTitle size="small">Lab 05 - โดย ศุจีภรณ์ รหัส 663380613-0</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+               <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
+        {/* CHANGE: Add the floating action button */}
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => addNewToGallery()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+
+        {/* CHANGE: Remove or comment out `ExploreContainer` */}
+        {/* <ExploreContainer name="Tab 2 page" /> */}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+
+export default Tab2;
